a93762921e96f7d2c680533e91d4b9ea583d1fac
Manoj Kumar <manojkp124@gmail.com>
2023-05-22 14:17:30 +0530

    added .env file

--- src/main/java/com/kumarmanoj/ecommerce/ECommerceApplication.java
+++ src/main/java/com/kumarmanoj/ecommerce/ECommerceApplication.java
@@ -1,11 +1,14 @@
 package com.kumarmanoj.ecommerce;
 
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PostConstruct;
+
 @SpringBootApplication
 public class ECommerceApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(ECommerceApplication.class, args);
     }
